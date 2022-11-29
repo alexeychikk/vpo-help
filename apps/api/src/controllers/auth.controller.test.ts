39a7e49dfe48238417b7e509c64a80abec213e6e
Alex Zinkevych <alexdarkmm@gmail.com>
2022-11-29 06:31:02 +0200

    test: auth as vpo

--- apps/api/src/controllers/auth.controller.test.ts
+++ apps/api/src/controllers/auth.controller.test.ts
@@ -1,7 +1,7 @@
 import { expectExtended } from '@vpo-help/testing';
 import { TEST_PASSWORD, testApp } from '../../testing';
 
-describe('POST /login', () => {
+describe('POST /auth/login', () => {
   test('rejects invalid body', async () => {
     const { body } = await testApp.requestApi
       .post('/auth/login')
@@ -58,6 +58,65 @@ describe('POST /login', () => {
         updatedAt: expectExtended.dateISOString(),
         email: user.email,
         role: user.role,
+      },
+    });
+  });
+});
+
+describe('POST /auth/login/vpo', () => {
+  test('rejects invalid body', async () => {
+    const { body } = await testApp.requestApi
+      .post('/auth/login/vpo')
+      .send({})
+      .expect(400);
+
+    expect(body).toMatchInlineSnapshot(`
+      Object {
+        "error": "Bad Request",
+        "message": Array [
+          "vpoReferenceNumber must be longer than or equal to 3 characters",
+        ],
+        "statusCode": 400,
+      }
+    `);
+  });
+
+  test('rejects invalid vpo reference number', async () => {
+    const { body } = await testApp.requestApi
+      .post('/auth/login/vpo')
+      .send({
+        vpoReferenceNumber: 'invalid_reference',
+      })
+      .expect(404);
+
+    expect(body).toMatchInlineSnapshot(`
+      Object {
+        "message": "Not Found",
+        "statusCode": 404,
+      }
+    `);
+  });
+
+  test('authenticates successfully', async () => {
+    const vpo = await testApp.asVpo().getCurrentVpo();
+
+    const { body } = await testApp.requestApi
+      .post('/auth/login/vpo')
+      .send({ vpoReferenceNumber: vpo.vpoReferenceNumber })
+      .expect(200);
+
+    expect(body).toMatchObject({
+      accessToken: {
+        access_token: expectExtended.accessToken(),
+      },
+      permissions: expect.any(Object),
+      user: {
+        id: expectExtended.objectId(),
+        createdAt: expectExtended.dateISOString(),
+        updatedAt: expectExtended.dateISOString(),
+        role: vpo.role,
+        scheduleDate: vpo.scheduleDate,
+        vpoReferenceNumber: vpo.vpoReferenceNumber,
       },
     });
   });
