5ff32354929555db7a94f16fcf4f97fcc3ea7bf2
Alex Zinkevych <alexdarkmm@gmail.com>
2023-01-20 02:58:16 +0200

    fix: proper env for client ci

--- apps/client/webpack.config.js
+++ apps/client/webpack.config.js
@@ -1,12 +1,17 @@
-const Dotenv = require('dotenv-webpack');
+const dotenv = require('dotenv');
+const webpack = require('webpack');
 
 module.exports = (config) => {
+  dotenv.config({
+    path: config.mode === 'production' ? '.env' : 'apps/client/.env.dev',
+  });
   return {
     ...config,
     plugins: [
       ...config.plugins,
-      new Dotenv({
-        path: config.mode === 'production' ? '.env' : 'apps/client/.env.dev',
+      new webpack.DefinePlugin({
+        'process.env.CLIENT_ENV': JSON.stringify(process.env.CLIENT_ENV),
+        'process.env.API_URL': JSON.stringify(process.env.API_URL),
       }),
     ],
   };
