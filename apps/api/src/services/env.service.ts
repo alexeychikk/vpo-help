282fb6a46265e515105a84eafab4f088066353cf
Alex Zinkevych <alexdarkmm@gmail.com>
2023-02-03 16:45:44 +0200

    chore: increase memory limit for prod

--- apps/api/src/services/env.service.ts
+++ apps/api/src/services/env.service.ts
@@ -4,6 +4,6 @@ import { EnvBaseService } from '@vpo-help/server';
 @Injectable()
 export class EnvService extends EnvBaseService {
   get MEMORY_LIMIT() {
-    return (this.IS_PROD ? 256 : 2048) * 1024 * 1024;
+    return (this.IS_PROD ? 500 : 2048) * 1024 * 1024;
   }
 }
